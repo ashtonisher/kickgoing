d1f8f760b21197af77704b5cf2b9996c8ba2636f
ashtonisher <cosmicpsy@gmail.com>
2021-08-09 18:07:11 +0900

    bxslider 구현

---
+++ js/slideArrowOnClick.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+  //service_btn 클릭시 슬라이드 좌우 이동
+  $('.service_btn').on('click', btnClick);
+
+  //service_arrow 버튼 클릭시 좌우 이동
+  var slideIndex = 0;
+  $('.service_arrow').on('click', function () {
+    imgWidth = $('.service_bg').width();
+    var slidePosition = $('#service_wrapper').css('right');
+    //left_arrow 클릭
+    if ($(this).hasClass('left_arrow')) {
+      if (slidePosition == imgWidth + 'px') {
+        //처음 이미지일 때, 방향 전환없이 마지막 (2번) 이미지로 자연스럽게 이어지도록
+        slideIndex = 3;
+        $('#service_wrapper').animate({ right: 0 }, 500, 'linear', function () {
+          $('#service_wrapper').css('right', slideIndex * imgWidth);
+        });
+        // setTimeout(function () {
+        //   $('#service_wrapper').css('right', 2 * imgWidth);
+        // }, 500);
+      } else {
+        --slideIndex;
+        $('#service_wrapper').animate(
+          { right: imgWidth * slideIndex },
+          500,
+          'linear'
+        );
+      }
+    } else {
+      //right_arrow 클릭
+      if (slidePosition == 3 * imgWidth + 'px') {
+        //마지막 이미지일 때, 방향 전환없이 첫번째 이미지로 자연스럽게 이어지도록
+        slideIndex = 1;
+        $('#service_wrapper').animate(
+          { right: 4 * imgWidth },
+          500,
+          'linear',
+          function () {
+            $('#service_wrapper').css('right', slideIndex * imgWidth);
+          }
+        );
+      } else {
+        ++slideIndex;
+        $('#service_wrapper').animate(
+          { right: imgWidth * slideIndex },
+          500,
+          'linear'
+        );
+      }
+    }
+  });
+});
+
+function btnClick() {
+  var btnOn = $(this).hasClass('btn_selected');
+  var btnIndex = $(this).index();
+  var imgWidth = $('.service_bg').width();
+  if (btnOn) {
+  } else {
+    $('.service_btn').removeClass('btn_selected');
+    $(this).addClass('btn_selected');
+  }
+  $('#service_wrapper').animate(
+    { right: (btnIndex + 1) * imgWidth },
+    500,
+    'linear'
+  );
+}
